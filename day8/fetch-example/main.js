d9f8591b51843f1a5282f88277a5e69239ac99a8
mariafe <mariafe@umich.edu>
2022-07-21 09:59:56 -0300

    updated day8

---
+++ day8/fetch-example/main.js
@@ -0,0 +1,57 @@
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+async function fetchPosts() {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  return response.json();
+}
+
+function printPosts(posts) {
+  let html = '<ul>';
+  for (let post of posts) {
+    html += '<li>' + post.title + '</li>';
+  }
+  html += '</ul>';
+  document.body.innerHTML = html;
+}
+
+async function fetchAndPrintPosts() {
+  try {
+    const posts = await fetchPosts();
+
+    printPosts(posts);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+async function createPost(post) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(post)
+  });
+
+  return response.json();
+}
+
+fetchAndPrintPosts();
+
+const newPost = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+};
+createPost(newPost).then(result => {
+  console.log(result);
+}).catch(err => {
+  console.log(err);
+})
